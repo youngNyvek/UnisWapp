c48043a6ceb0d4e861b593d79b361e53d0bd4bd6
Kevin Faria <kevintxd73@gmail.com>
2021-04-11 15:28:11 -0300

    Colocando o app modelo como inicio do projeto, para manter o ensinado pelo professor

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import { View, StyleSheet, Text, StatusBar } from 'react-native';
-import UniCurrency from './services/api';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-    const [brl, setBrl] = useState(Number);
+import E from "./estilo";
 
-    useEffect( ()=>{
-        UniCurrency().then( response => {
-            setBrl(response.data.uniswap.brl);
-        });
-    },[])
-    return (
-        <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-            <View style={styles.container}> 
-                <Text style={styles.title}>UnisWApp</Text>
-                <Text style={styles.title}>Cotação</Text>
-                <Text style={styles.title}>{brl}</Text>
-                <Text style={styles.description}>Aplicativo do trabalho de Dev Mobile</Text>
-            </View>
-        </>
-    )
+export default function App09() {
+return (
+<View style={E.app}>
+<Text style={E.titulo}>App-09</Text>
+</View>
+);
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#7159c1',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-
-    title: {
-        color: '#FFF',
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-
-    description: {
-        color: '#FFF',
-        fontSize: 12,
-        fontWeight: 'bold'
-    }
-})
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const UniCurrency = () => {
-    return axios.get('https://api.coingecko.com/api/v3/simple/price?ids=uniswap&vs_currencies=brl');
-}
-
-export default UniCurrency;
